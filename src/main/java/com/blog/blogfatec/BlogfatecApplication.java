d34689b0c212fe5631448a56531a1cfe4c0ce3a9
SLLGentile <sergiogentilesi@gmail.com>
2024-09-26 19:38:02 -0300

    inicio

---
+++ src/main/java/com/blog/blogfatec/BlogfatecApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogfatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogfatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogfatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/blogfatec/BlogfatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogfatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogfatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
